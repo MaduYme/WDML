0c5d46214f2a91435df5cdd430449d65a0287c5b
Madusanka Gamage <madu.yme@gmail.com>
2024-11-26 06:59:19 +0530

    Update scrpts..js
    date and time

--- scrpts..js
+++ scrpts..js
@@ -101,6 +101,17 @@ if (document.getElementById('stockChart')) {
                     position: 'top',
                 },
             }
+            function updateClock() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
         }
     });
 }
